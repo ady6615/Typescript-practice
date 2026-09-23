a0017a5b4b81bcbd67b39b7a5b6d99565a9c64c6
ady6615 <advaitpatil379@gmail.com>
2026-09-23 20:45:12 +0530

    first variables

---
+++ variables.js
@@ -0,0 +1,8 @@
+let greetings = "HEllo World";
+console.log(greetings);
+let n1 = 1;
+let n2 = 2;
+let sum;
+sum = 1 + 4 + n2 - n1;
+console.log(sum);
+export {};
---
+++ variables.ts
@@ -0,0 +1,14 @@
+let greetings:string = "HEllo World"
+
+console.log (greetings)
+
+let n1 = 1
+let n2 = 2
+let sum
+sum = 1+4+n2-n1
+
+console.log(sum)
+
+
+//to get rid of ~~ lines
+export{}
