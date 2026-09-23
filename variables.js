9406b5594463ce31ceac0f1a0235d6b551b23912
ady6615 <advaitpatil379@gmail.com>
2026-09-23 22:32:47 +0530

    automated data type assigning

--- variables.js
+++ variables.js
@@ -4,5 +4,11 @@ let n1 = 1;
 let n2 = 2;
 let sum;
 sum = 1 + 4 + n2 - n1;
+//showing how TS automatically assigns type to the variable based on its value
 console.log(sum);
+let Id2 = 1283;
+let Id3 = 460;
+sum = Id2 + Id3;
+console.log(sum);
+
 export {};
--- variables.ts
+++ variables.ts
@@ -7,7 +7,16 @@ let n2 = 2
 let sum
 sum = 1+4+n2-n1
 
+
+//showing how TS automatically assigns type to the variable based on its value
 console.log(sum)
+let Id2:number = 1283
+let Id3 = 460
+
+sum = Id2 + Id3
+console.log(sum)
+
+
 
 
 //to get rid of ~~ lines
